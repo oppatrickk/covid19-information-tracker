ca8b7b9ab4e4c1fc395afb5c1df2be2872939443
John Patrick Prieto <70645552+oppatrickk@users.noreply.github.com>
2021-04-07 07:30:34 +0800

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:covid19_information_center/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 // Widgets
 import 'package:covid19_information_center/widgets/app_bar.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
